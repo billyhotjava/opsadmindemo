76a00b5bf367a93c46bb83884dec71a028ee3d2c
billy <billy.xiezhimin@gmail.com>
2023-04-10 17:25:48 +0800

    update to log branch

--- src/main/java/com/yzcloud/ops/admin/repository/AlarmInfoRepository.java
+++ src/main/java/com/yzcloud/ops/admin/repository/AlarmInfoRepository.java
@@ -11,21 +11,4 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface AlarmInfoRepository extends JpaRepository<AlarmInfo, Long> {
-    @Query(
-        nativeQuery = true,
-        value = " select t1.* \n" +
-        " from alarm_info t1\n" +
-        " join alarm_rule t2 on t1.alarm_rule_id = t2.id\n" +
-        " join event_rule t3 on t2.event_rule_id = t3.id\n" +
-        " join category t4 on t3.category_id = t4.id\n" +
-        " where t2.id  = :alarmRuleId\n" +
-        " and t3.id  = :eventRuleId\n" +
-        " and t4.id = :categoryId"
-    )
-    List<AlarmInfo> findAllAlarmInfoByQuery(
-        @Param("alarmRuleId") Long alarmRuleId,
-        @Param("eventRuleId") Long eventRuleId,
-        @Param("categoryId") Long categoryId
-    );
-}
+public interface AlarmInfoRepository extends JpaRepository<AlarmInfo, Long>, JpaSpecificationExecutor<AlarmInfo> {}
